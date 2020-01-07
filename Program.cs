f6398c6a850256147a398bca728ad45f24704b50
Pankaj Sharma <pankaj073@gmail.com>
2020-01-07 20:59:36 +0530

    Created a new console application:

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace deniskyashif_learn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
