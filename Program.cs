149b4970c2241c6ae6acdd862cb39b502309c5a7
Pankaj Sharma <pankaj073@gmail.com>
2020-01-07 21:58:05 +0530

    basic channels

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
 
 namespace deniskyashif_learn
 {
@@ -6,7 +8,27 @@ namespace deniskyashif_learn
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
+            try 
+            {
+                ChannelsDemo().GetAwaiter().GetResult();
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine($"There was an exception: {ex.ToString()}");
+            }
+        }
+
+        static async Task<int> ChannelsDemo() 
+        {
+            Channel<string> ch = Channel.CreateUnbounded<string>();
+            await ch.Writer.WriteAsync("My first message");
+            await ch.Writer.WriteAsync("My second message");
+            ch.Writer.Complete();
+
+            while(await ch.Reader.WaitToReadAsync()) 
+                Console.WriteLine(await ch.Reader.ReadAsync());
+            return 0;
         }
     }
 }
