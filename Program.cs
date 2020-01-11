7c286f1e79ac12b28e43e3fbb50be3f8789f7b82
Pankaj Sharma <pankaj073@gmail.com>
2020-01-11 23:33:09 +0530

    Implemented merge

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Channels;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace deniskyashif_learn
 {
@@ -21,24 +22,94 @@ namespace deniskyashif_learn
 
         static async Task ChannelsDemo() 
         {
-            var ch = Channel.CreateUnbounded<string>();
+            var joe = CreateMessenger("Joe", 5);
+            var ann = CreateMessenger("Ann", 5);
 
-            var consumer = Task.Run(async () => 
+            /*
+            We ensure that our consumers won't be able to attempt
+            writing to it.
+
+            await foreach(var item in joe.ReadAllAsync())
+                Console.WriteLine(item);
+            */
+
+            /*
+            Suppose Ann sends more messages than Joe, We're still going to try
+            and read from Joe, even when his channel is completed which is going to
+            throw an exception.
+
+            while(await joe.WaitToReadAsync() || await ann.WaitToReadAsync())
             {
-                while(await ch.Reader.WaitToReadAsync())
-                    Console.WriteLine(await ch.Reader.ReadAsync());
-            });
+                Console.WriteLine(await joe.ReadAsync());
+                Console.WriteLine(await ann.ReadAsync());
+            }
+            */
 
-            var producer = Task.Run(async () => {
-               var rnd = new Random();
-               for(int i = 0; i < 5; i++) {
-                   await Task.Delay(TimeSpan.FromSeconds(rnd.Next(3)));
-                   await ch.Writer.WriteAsync($"Message {i}");
-               } 
-               ch.Writer.Complete();
-            });
+            /*
+            This will force us to wait for Joe, even though we might have
+            several messages waiting ready to be read from Ann. Currently, we 
+            cannot read from Ann before reading from Joe.
 
-            await Task.WhenAll(producer, consumer);
+            try 
+            {
+                Console.WriteLine(await joe.ReadAsync());
+                Console.WriteLine(await ann.ReadAsync());
+            }
+            catch(ChannelClosedException) { }
+            */
+
+            var ch = Merge(joe, ann);
+            await foreach(var item in ch.ReadAllAsync())
+                Console.WriteLine(item);
+        }
+
+        static ChannelReader<string> CreateMessenger(string msg, int count) 
+        {
+            var ch = Channel.CreateUnbounded<string>();
+            var rnd = new Random();
+
+            Task.Run(async () => 
+            { 
+                for(int i = 0; i < count; i++)
+                {
+                    await ch.Writer.WriteAsync($"{msg} {i}");
+                    await Task.Delay(TimeSpan.FromSeconds(rnd.Next(3)));
+                }
+                ch.Writer.Complete();
+            });
+            return ch.Reader;
+        }
+
+        static ChannelReader<T> Merge<T>(ChannelReader<T> first, ChannelReader<T> second)
+        {
+            var output = Channel.CreateUnbounded<T>();
+            Task.Run(async () => 
+            {
+                await foreach(var item in first.ReadAllAsync())
+                    await output.Writer.WriteAsync(item);
+            });
+            Task.Run(async () => {
+                await foreach(var item in second.ReadAllAsync())
+                    await output.Writer.WriteAsync(item);
+            });
+            return output;
+        }
+
+        static ChannelReader<T> Merge<T>(params ChannelReader<T>[] inputs) 
+        {
+            var output = Channel.CreateUnbounded<T>();
+            Task.Run(async () => 
+            {
+                async Task Redirect(ChannelReader<T> input)
+                {
+                    await foreach (var item in input.ReadAllAsync())
+                        await output.Writer.WriteAsync(item);
+                }
+
+                await Task.WhenAll(inputs.Select(i => Redirect(i)).ToArray());
+                output.Writer.Complete();
+            });
+            return output;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # deniskyashif_learn
 Learnings from deniskyashif blog
+
+## The Generator
+A generator is a method that returns a channel.
